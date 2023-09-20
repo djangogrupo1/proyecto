9bdca0a287b976a1bbbd489d63915fef3b75e5e9
cristiangomezdev <cristiangomez.dev@gmail.com>
2023-09-20 12:42:58 -0300

    agregue el html base y home/index

--- gesFar/Farmacia/urls.py
+++ gesFar/Farmacia/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.shortcuts import render
 
 urlpatterns = [
     path("", views.index, name="index"),
--- gesFar/gesFar/urls.py
+++ gesFar/gesFar/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("grupo1/" , include("Farmacia.urls")),   
+    path("" , include("Farmacia.urls")),   
 ]
