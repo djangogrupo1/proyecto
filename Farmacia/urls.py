7a84e719dcab98fa323c32b2411c083ec6a5e72a
xtsulyts <126834739+xtsulyts@users.noreply.github.com>
2023-09-27 10:53:02 -0300

    agrego path contacto/ a urls.py

--- Farmacia/urls.py
+++ Farmacia/urls.py
@@ -4,11 +4,9 @@ from django.shortcuts import render
 
 urlpatterns = [
     path("", views.index, name="index"),
-<<<<<<< HEAD:gesFar/Farmacia/urls.py
     path("acercade/", views.acercade, name="acercade"),
-    path("contacto/", views.contacto, name="contacto")
-=======
+    path("contacto/", views.contacto, name="contacto"),
     path("modulo/", views.modulo, name="modulo"),
     path("acercade/", views.acercade, name="acercade")
->>>>>>> 0e8b3dd48968319275e972e7c7ab5a53b0a7dbb8:Farmacia/urls.py
-]
+         
+    ]
