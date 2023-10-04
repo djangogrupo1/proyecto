0fb945a58c70563cec10a99cbc39f43d92983137
Victoria Avalos <inessavalos14@gmail.com>
2023-10-04 17:44:42 -0300

    probando

--- Farmacia/views.py
+++ Farmacia/views.py
@@ -13,10 +13,7 @@ def modulo(request):
 
 def acercade(request):
     return render(request, "acercade.html")
-<<<<<<< HEAD:gesFar/Farmacia/views.py
 
 def contacto (request):
     return render(request, "contacto.html")
 
-=======
->>>>>>> 0e8b3dd48968319275e972e7c7ab5a53b0a7dbb8:Farmacia/views.py
