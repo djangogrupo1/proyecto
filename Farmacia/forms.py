a40084da41463c9ba1ea79f2ed33cd2d711b1add
xtsulyts <xtsulyts@fedora>
2023-10-05 13:57:12 -0400

    agrego validaciones a form.py

--- Farmacia/forms.py
+++ Farmacia/forms.py
@@ -4,41 +4,22 @@ from django.core.exceptions import ValidationError
 class ContactoForm ( forms.Form ):
     nombre = forms.CharField (
         label= "Nombre:", required = True
-
-        )
+          )
     apellido = forms.CharField (
-        label= "Apellido:", required = True, widget = forms.TextInput(attrs={'class': 'form-control'})
-
-        )
+        label= "Apellido:", required = True, widget = forms.TextInput(attrs={'class': 'form-control'}))
+    
     email = forms.EmailField (
-        label= "Email:", required = True
-        
-        )
+        label= "Email:", required = True )
 
     mensaje = forms.CharField(
-    label= "Mensaje", required = False, widget=forms.Textarea
-    )
+    label= "Mensaje", required = False, widget=forms.Textarea)
 
     def clean_nombre(self):
-       if self.cleaned_data["nombre"] == int :
-          raise ValueError ("palabra inapropida")
+       if self.cleaned_data["nombre"] == "odio" :
+          raise  ValidationError ("palabra inapropida")
        return self.cleaned_data["nombre"]
-    
         
-              
-
-
-
-
-
-
     def clean(self):
-      pass     
-    
-      
-  
-
-
-    #edad = forms.IntegerField (label= "Edad:" min_value=1 , max_value=90)          
-    
-    
+      if self.cleaned_data["nombre"] == "odio" and  self.cleaned_data["apellido"] == "terror":
+         raise ValidationError ("palabras imapropiadas")
+      return self.cleaned_data
