f9bafb6f805bfe74323c8027f57c1db3ae77c125
Walter <531xtsulyts@gmail.com>
2023-10-07 18:37:01 -0300

    trabajando en forms

--- Farmacia/forms.py
+++ Farmacia/forms.py
@@ -3,10 +3,10 @@ from django.core.exceptions import ValidationError
 
 class ContactoForm ( forms.Form ):
     nombre = forms.CharField (
-        label= "Nombre:", required = True
-          )
+        label= "Nombre:", required = True, widget= forms.TextInput)
+    
     apellido = forms.CharField (
-        label= "Apellido:", required = True, widget = forms.TextInput)#(attrs={'class': 'form-control'}))
+        label= "Apellido:", required = True, widget = forms.TextInput(attrs={'class': 'form_estilo'}))
     
     email = forms.EmailField (
         label= "Email:", required = True )
@@ -14,12 +14,12 @@ class ContactoForm ( forms.Form ):
     mensaje = forms.CharField(
     label= "Mensaje", required = False, widget=forms.Textarea)
 
-    """def clean_nombre(self):
+    def clean_nombre(self):
        if self.cleaned_data["nombre"] == "odio" :
           raise  ValidationError ("palabra inapropida")
-       return self.cleaned_data["nombre"]"""
+       return self.cleaned_data["nombre"]
         
     def clean(self):
-      if self.cleaned_data["nombre"] == "odio" and  self.cleaned_data["apellido"] == "terror":
+      if self.cleaned_data["nombre"] == "" and  self.cleaned_data[""] == "terror":
          raise ValidationError ("palabras inapropiadas")
       return self.cleaned_data
