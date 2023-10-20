38a04242110a48fde18cb3f9d8f81217be03eac9
cristiangomezdev <cristiangomez.dev@gmail.com>
2023-10-20 12:07:14 -0300

    fix:view,form and contacto.html

--- Farmacia/forms.py
+++ Farmacia/forms.py
@@ -2,61 +2,69 @@ from django import forms
 from django.forms import ValidationError
 import re
 
-##validaciones##
+## Validaciones ##
 def no_numeros(value):
-   if any(char.isdigit() for char in value):
-      raise ValidationError (
-         'El campo no permite numeros, %(valor)s',code='Ivalid',params={'valor': value})
-   
+    if any(char.isdigit() for char in value):
+        raise ValidationError('El campo no permite números: %(valor)s', code='invalid', params={'valor': value})
+
 def no_carateres_reg(value):
     re_email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     if not re.match(re_email, value):
         raise ValidationError('Correo electrónico inválido')
-    
-def no_letras (value):
-   if any(char.isalfa() for char in value):
-      raise ValidationError ("No se permiten letras en este campo")
 
-   
-   
-class ContactoForm ( forms.Form ):
-    nombre = forms.CharField (
-        label= "Nombre:", required = True, 
-        validators= (no_numeros,) and (no_carateres_reg,),
-        widget= forms.TextInput)
-    
-    apellido = forms.CharField (
-        label= "Apellido:", required = True,
-        validators= (no_numeros,) and (no_carateres_reg,),
-        widget = forms.TextInput)
-    
-    email = forms.EmailField (
-        label= "Email:", required = True,
-        validators= (no_carateres_reg,),
-        widget= forms.EmailInput)
-        
+def no_letras(value):
+    if any(char.isalpha() for char in value):
+        raise ValidationError('No se permiten letras en este campo')
+
+class ContactoForm(forms.Form):
+    nombre = forms.CharField(
+        label="Nombre:",
+        required=True,
+        validators=[no_numeros],
+        widget=forms.TextInput
+    )
+
+    apellido = forms.CharField(
+        label="Apellido:",
+        required=True,
+        validators=[no_numeros],
+        widget=forms.TextInput
+    )
+
+    email = forms.EmailField(
+        label="Email:",
+        required=True,
+        validators=[no_carateres_reg],
+        widget=forms.EmailInput
+    )
+
     mensaje = forms.CharField(
-    label= "Mensaje:", required = False, widget=forms.Textarea)
+        label="Mensaje:",
+        required=False,
+        widget=forms.Textarea
+    )
 
-    ##CLEANS##
-
+    ## Cleans ##
     def clean_nombre(self):
-       if self.cleaned_data["nombre"] == "odio":
-        raise  ValidationError ("Palabra inapropiada")
-       return self.cleaned_data["nombre"]
+        if self.cleaned_data["nombre"] == "odio":
+            raise ValidationError("Palabra inapropiada")
+        return self.cleaned_data["nombre"]
 
     def clean_apellido(self):
-       if self.cleaned_data["apellido"] == "terror":
-        raise  ValidationError ("Palabra inapropiada")
-       return self.cleaned_data["nombre"]
-    
+        if self.cleaned_data["apellido"] == "terror":
+            raise ValidationError("Palabra inapropiada")
+        return self.cleaned_data["apellido"]
+
     def clean_mensaje(self):
-        if self.cleaned_data['mensaje'] <5:
-            raise ValidationError(
-                "Debes especificar mejor el mensaje que nos envias")
-        return self.cleaned_data['mensaje']
+        mensaje = self.cleaned_data['mensaje']
+        if len(mensaje) < 5:
+            raise ValidationError("Debes especificar mejor el mensaje que nos envías")
+        return mensaje
 
     def clean(self):
-      if self.cleaned_data["nombre"] == "" and  self.cleaned_data[""] == "terror":
-         raise ValidationError ("palabras inapropiadas")
-      return self.cleaned_data
+        cleaned_data = super().clean()
+        nombre = cleaned_data.get("nombre")
+        apellido = cleaned_data.get("apellido")
+        if nombre == "odio" and apellido == "terror":
+            raise ValidationError("Palabras inapropiadas")
+        return cleaned_data
--- Farmacia/views.py
+++ Farmacia/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.http import HttpResponse, HttpResponseRedirect
-from  Farmacia.forms import ContactoForm
+from Farmacia.forms import ContactoForm
 from django.urls import reverse
 from django.contrib import messages
-import sys
 from Farmacia.models import Contacto
 import os
 
-sys.path.append('tp_cac_23635_version2')
+# Asumiendo que 'tp_cac_23635_version2' se encuentra en el mismo directorio que tu proyecto
 
 # Create your views here.
 def index(request):
@@ -24,43 +23,37 @@ def acercade(request):
 def nosotros(request):
     return render(request, "nosotros.html")
 
-def contacto(request, ):
-  formulario = None
-  if request.method == 'POST':
-      
-      formulario = ContactoForm ( request.POST )
-      if formulario.is_valid ():
-        messages.success(request, 'Recibimos tu mensaje')
-        mensaje = f"De : {formulario.cleaned_data['nombre']} <{formulario.cleaned_data['apellido']}>\n Asunto: {formulario.cleaned_data['email']}\n Mensaje: {formulario.cleaned_data['mensaje']}"
-        mensaje_html = f"""
-                <p>De: {formulario.changed_data['nombre']} <a href="mailto:{formulario.cleaned_data['apellido']}">{formulario.cleaned_data['email']}</a></p>
-                <p>Asunto:  {formulario.cleaned_data['asunto']}</p>
+def contacto(request):
+    formulario = None
+    if request.method == 'POST':
+        formulario = ContactoForm(request.POST)
+        if formulario.is_valid():
+            messages.success(request, 'Recibimos tu mensaje. Pronto te responderemos.')
+            mensaje = f"De: {formulario.cleaned_data['nombre']} {formulario.cleaned_data['apellido']} <{formulario.cleaned_data['email']}>\nMensaje: {formulario.cleaned_data['mensaje']}"
+            mensaje_html = f"""
+                <p>De: {formulario.cleaned_data['nombre']} <a href="mailto:{formulario.cleaned_data['email']}">{formulario.cleaned_data['email']}</a></p>
                 <p>Mensaje: {formulario.cleaned_data['mensaje']}</p>
             """
             
-        contacto_db = Contacto (
-            nombre = formulario.cleaned_data ["nombre"],
-            apellido = formulario.cleaned_data ["apellido"],
-            email = formulario.cleaned_data ["email"],
-            mensaje = formulario.cleaned_data ["mensaje"]
-        )
+            contacto_db = Contacto(
+                nombre=formulario.cleaned_data["nombre"],
+                apellido=formulario.cleaned_data["apellido"],
+                email=formulario.cleaned_data["email"],
+                mensaje=formulario.cleaned_data["mensaje"]
+            )
 
-        contacto_db.save()
+            contacto_db.save()
 
-        return redirect(reverse(request, 'index'))
+            return redirect('index')  # 'index' es un nombre de URL, no necesita reverse
 
-      else:
-        messages.error(request, 'Error al cargar formulario')
+        else:
+            messages.error(request, 'Error al cargar formulario')
 
-        
-  else:
-      formulario = ContactoForm ()
+    else:
+        formulario = ContactoForm()
          
-  context =  {
-     'formulario_contacto'  : formulario 
-      }
-  return render (request, "contacto.html", context ) 
-
+    context = {'formulario_contacto': formulario}
+    return render(request, "contacto.html", context)
 
 ####se definen cleans###
 
