b221e827eab23eed8939c45b178b9e302733761b
Victoria Avalos <inessavalos14@gmail.com>
2023-11-16 18:45:07 -0300

    modificando panel de admin

--- gesFar/settings.py
+++ gesFar/settings.py
@@ -1,7 +1,7 @@
 
 from pathlib import Path
 
-from decouple import  config
+from decouple import config
 """
 Django settings for gesFar project.
 
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
